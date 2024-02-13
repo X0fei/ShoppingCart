994dc8644e69a58013c9e781665edde1d5c86533
X0fei <pitonvano05@gmail.com>
2024-02-13 10:06:08 +0300

    Project created

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ShoppingCart
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
