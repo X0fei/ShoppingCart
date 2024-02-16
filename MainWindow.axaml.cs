0a762b9173bff6dc9013b0e9ca153a5024b79a7e
X0fei <pitonvano05@gmail.com>
2024-02-16 11:38:04 +0300

    feat: implement add products in shopping cart

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,12 +1,35 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ShoppingCart
 {
     public partial class MainWindow : Window
-    {
+    {   
+        private List<Products> products = new List<Products>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public void ProductAdd(object source, RoutedEventArgs args)
+        {
+            bool empty = false;
+            for (int i = 0; i < productName.Text.Length; i++)
+            {
+                if (productName.Text[i] == ' ')
+                {
+                    empty = true;
+                }
+            }
+            if (productName.Text != null && productName.Text != "" && empty == false)
+            {
+                products.Add(new Products()
+                {
+                    ProductNameSC = productName.Text
+                });
+                shoppingCart.ItemsSource = products.ToList();
+            }
         }
     }
 }
--- obj/Debug/net6.0/ShoppingCart.AssemblyInfo.cs
+++ obj/Debug/net6.0/ShoppingCart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+994dc8644e69a58013c9e781665edde1d5c86533")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
