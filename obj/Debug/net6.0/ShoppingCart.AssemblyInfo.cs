de10ce4c8f8462c8e3386292f90f17680f54ccad
MalProk <darova@mail.ru>
2024-04-02 12:51:54 +0300

    start work on scroll bar

--- obj/Debug/net6.0/ShoppingCart.AssemblyInfo.cs
+++ obj/Debug/net6.0/ShoppingCart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38a2094deaaf8973838c882dc31e05d9e75f7361")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75a495a5a1c11a0e720f0120ba24a4c797b864ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
